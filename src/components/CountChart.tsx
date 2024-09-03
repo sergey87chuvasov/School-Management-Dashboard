dd0303d21e6e3050d0d01883cb631ab2a92d5912
Sergey87 <serge.techno@gmail.com>
2024-09-04 00:01:37 +0300

    start work with charts

---
+++ src/components/CountChart.tsx
@@ -0,0 +1,100 @@
+'use client';
+
+import Image from 'next/image';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts';
+
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
+const CountChart = () => {
+  return (
+    <div className='bg-white rounded-xl w-full h-full p-4'>
+      <div className='flex justify-between items-center'>
+        <h1>Students</h1>
+        <Image src='/moreDark.png' alt='icon pic' width={20} height={20} />
+      </div>
+      <div>
+        {/* <ResponsiveContainer width='100%' height='100%'>
+          <LineChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray='3 3' />
+            <XAxis dataKey='name' />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line
+              type='monotone'
+              dataKey='pv'
+              stroke='#8884d8'
+              activeDot={{ r: 8 }}
+            />
+            <Line type='monotone' dataKey='uv' stroke='#82ca9d' />
+          </LineChart>
+        </ResponsiveContainer> */}
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default CountChart;
