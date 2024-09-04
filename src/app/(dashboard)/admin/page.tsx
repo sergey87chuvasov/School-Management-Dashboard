71687141448538bbbff7fc59048dcc0d438c3ae7
Sergey87 <serge.techno@gmail.com>
2024-09-05 00:22:21 +0300

    work with charts - part 4

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceChart from '@/components/AttendanceChart';
 import CountChart from '@/components/CountChart';
+import FinanceChart from '@/components/FinanceChart';
 import UserCard from '@/components/UserCard';
 
 const AdminPage = () => {
@@ -20,7 +21,9 @@ const AdminPage = () => {
             <AttendanceChart />
           </div>
         </div>
-        <div className=''></div>
+        <div className='w-full h-[500px]'>
+          <FinanceChart />
+        </div>
       </div>
       <div className='w-full lg:w-1/3'>r</div>
     </div>
