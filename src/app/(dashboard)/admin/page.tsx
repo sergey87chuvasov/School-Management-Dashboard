ceb673778599998894d26c6eb99f70bc3af03bd6
Sergey87 <serge.techno@gmail.com>
2024-09-05 00:04:30 +0300

    work with charts - part 3

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,4 @@
+import AttendanceChart from '@/components/AttendanceChart';
 import CountChart from '@/components/CountChart';
 import UserCard from '@/components/UserCard';
 
@@ -15,7 +16,9 @@ const AdminPage = () => {
           <div className='w-full lg:w-1/3 h-[450px]'>
             <CountChart />
           </div>
-          <div className='w-full lg:w-2/3 h-[450px]'></div>
+          <div className='w-full lg:w-2/3 h-[450px]'>
+            <AttendanceChart />
+          </div>
         </div>
         <div className=''></div>
       </div>
---
+++ src/components/AttendanceChart.tsx
@@ -0,0 +1,87 @@
+'use client';
+
+import Image from 'next/image';
+import {
+  BarChart,
+  Bar,
+  Rectangle,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts';
+
+const data = [
+  {
+    name: 'Mon',
+    present: 60,
+    absent: 40,
+  },
+  {
+    name: 'Tue',
+    present: 70,
+    absent: 60,
+  },
+  {
+    name: 'Wed',
+    present: 90,
+    absent: 75,
+  },
+  {
+    name: 'Thu',
+    present: 60,
+    absent: 30,
+  },
+  {
+    name: 'Fri',
+    present: 35,
+    absent: 15,
+  },
+];
+
+const AttendanceChart = () => {
+  return (
+    <div className='bg-white rounded-lg p-4 h-full'>
+      <div className='flex justify-between items-center'>
+        <h1 className='text-lg font-semibold'>Attendance</h1>
+        <Image src='/moreDark.png' alt='icon pic' width={20} height={20} />
+      </div>
+      <ResponsiveContainer width='100%' height='90%'>
+        <BarChart width={500} height={300} data={data} barSize={20}>
+          <CartesianGrid strokeDasharray='3 3' vertical={false} stroke='#ddd' />
+          <XAxis
+            dataKey='name'
+            axisLine={false}
+            tick={{ fill: '#d1d5db' }}
+            tickLine={false}
+          />
+          <YAxis axisLine={false} tick={{ fill: '#d1d5db' }} tickLine={false} />
+          <Tooltip
+            contentStyle={{ borderRadius: '10px', borderColor: 'lightgray' }}
+          />
+          <Legend
+            align='left'
+            verticalAlign='top'
+            wrapperStyle={{ paddingTop: '20px', paddingBottom: '40px' }}
+          />
+          <Bar
+            dataKey='present'
+            fill='#fae27c'
+            legendType='circle'
+            radius={[10, 10, 0, 0]}
+          />
+          <Bar
+            dataKey='absent'
+            fill='#c3ebfa'
+            legendType='circle'
+            radius={[10, 10, 0, 0]}
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default AttendanceChart;
