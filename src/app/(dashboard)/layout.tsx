6185cb514c4433a334fe56f3eae316bf38755294
Sergey87 <serge.techno@gmail.com>
2024-09-10 00:21:26 +0300

    finished admin dashboard home page

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -16,7 +16,7 @@ export default function DashboardLayout({
           className='flex items-center justify-center lg:justify-start gap-2'
         >
           <Image src='/logo.png' alt='logo pic' width={32} height={32} />
-          <span className='hidden lg:block'>School</span>
+          <span className='hidden lg:block font-bold'>School</span>
         </Link>
         <Menu />
       </div>
