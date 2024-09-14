20cf1f4ff7e68c494ee90b71870ab86db63e7106
Sergey Chv <serge.techno@gmail.com>
2024-09-14 17:06:51 +0300

    added Student Dashboard Design

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,6 +1,19 @@
+import Announcements from "@/components/Announcements"
+import EventCalendar from "@/components/EventCalendar"
+
 const StudentPage = () => {
   return (
-    <div className=''>StudentPage</div>
+    <div className='p-4 flex gap-4 flex-col xl:flex-row'>
+      <div className="w-full xl:w-2/3">
+      <div className="h-full bg-white p-4 rounded-md">
+        <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+      </div>
+      </div>
+      <div className='w-full xl:w-1/3 flex flex-col gap-8'>
+        <EventCalendar />
+        <Announcements />
+      </div>
+    </div>
   )
 }
 
