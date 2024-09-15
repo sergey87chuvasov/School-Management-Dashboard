45be40a19f4cbfdcc823711155953631f9d05f1d
Sergey87 <serge.techno@gmail.com>
2024-09-16 00:02:03 +0300

    added homepage for each user

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -33,7 +33,7 @@ const EventCalendar = () => {
   const [value, onChange] = useState<Value>(new Date());
   return (
     <div className='bg-white p-4 rounded-md'>
-      <Calendar onChange={onChange} value={value} />
+      <Calendar onChange={onChange} value={value} locale='en-GB' />
       <div className='flex items-center justify-between'>
         <h1 className='text-xl font-semibold my-4'>Events</h1>
         <Image src='/moreDark.png' alt='img pic' width={20} height={20} />
