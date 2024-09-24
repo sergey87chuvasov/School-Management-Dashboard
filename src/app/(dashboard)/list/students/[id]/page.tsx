d98c9b487eef63a303e611b3fc03674eeb224381
Sergey87 <serge.techno@gmail.com>
2024-09-25 00:11:28 +0300

    added Single Student Page Design

--- src/app/(dashboard)/list/students/[id]/page.tsx
+++ src/app/(dashboard)/list/students/[id]/page.tsx
@@ -4,7 +4,7 @@ import Performance from '@/components/Perfomans';
 import Image from 'next/image';
 import Link from 'next/link';
 
-const SingleTeacherPage = () => {
+const SingleStudentPage = () => {
   return (
     <div className='flex-1 p-4 flex flex-col gap-4 xl:flex-row'>
       <div className='w-full xl:w-2/3'>
@@ -12,7 +12,7 @@ const SingleTeacherPage = () => {
           <div className='bg-lamaSky py-6 px-4 rounded-md flex-1 flex gap-4'>
             <div className='w-1/3'>
               <Image
-                src='https://images.pexels.com/photos/16928285/pexels-photo-16928285.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                src='https://images.pexels.com/photos/27641862/pexels-photo-27641862.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
                 alt='img pic'
                 width={144}
                 height={144}
@@ -20,10 +20,10 @@ const SingleTeacherPage = () => {
               />
             </div>
             <div className='w-2/3 flex flex-col justify-between gap-4'>
-              <h1 className='text-xl font-semibold'>Leonardo Snyder</h1>
+              <h1 className='text-xl font-semibold'>Cameron Moran</h1>
               <p className='text-sm text-gray-500'>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet
-                nostrum dolore quis porro tenetur? Veritatis esse officia.
+                nostrum dolore quis porro tenetur.
               </p>
               <div className='flex items-center justify-between gap-2 flex-wrap text-xs font-medium'>
                 <div className='w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2'>
@@ -88,8 +88,8 @@ const SingleTeacherPage = () => {
                 className='w-6 h-6'
               />
               <div>
-                <h1 className='text-xl font-semibold'>2</h1>
-                <span className='text-sm text-gray-400'>Branches</span>
+                <h1 className='text-xl font-semibold'>6th</h1>
+                <span className='text-sm text-gray-400'>Grade</span>
               </div>
             </div>
             <div className='w-full bg-white p-4 rounded-md flex gap-4 md:w-[48%] xl:w-[45%] 2xl:w-[48%]'>
@@ -101,7 +101,7 @@ const SingleTeacherPage = () => {
                 className='w-6 h-6'
               />
               <div>
-                <h1 className='text-xl font-semibold'>6</h1>
+                <h1 className='text-xl font-semibold'>18</h1>
                 <span className='text-sm text-gray-400'>Lessons</span>
               </div>
             </div>
@@ -114,14 +114,14 @@ const SingleTeacherPage = () => {
                 className='w-6 h-6'
               />
               <div>
-                <h1 className='text-xl font-semibold'>6</h1>
-                <span className='text-sm text-gray-400'>Classes</span>
+                <h1 className='text-xl font-semibold'>6A</h1>
+                <span className='text-sm text-gray-400'>Class</span>
               </div>
             </div>
           </div>
         </div>
         <div className='mt-4 bg-white rounded-md p-4 h-[800px]'>
-          <h1>Teacher&apos;s Schedule</h1>
+          <h1>Student&apos;s Schedule</h1>
           <BigCalendar />
         </div>
       </div>
@@ -130,19 +130,19 @@ const SingleTeacherPage = () => {
           <h1 className='text-xl font-semibold'>Shortcuts</h1>
           <div className='mt-4 flex gap-4 flex-wrap text-xs text-gray-500'>
             <Link className='p-3 rounded-md bg-lamaSkyLight' href='/'>
-              Teacher&apos;s Classes
+              Student&apos;s Lessons
             </Link>
             <Link className='p-3 rounded-md bg-lamaPurpleLight' href='/'>
-              Teacher&apos;s Students
+              Student&apos;s Teachers
             </Link>
             <Link className='p-3 rounded-md bg-lamaYellowLight' href='/'>
-              Teacher&apos;s Lessons
+              Student&apos;s Exams
             </Link>
             <Link className='p-3 rounded-md bg-pink-50' href='/'>
-              Teacher&apos;s Exams
+              Student&apos;s Assignments
             </Link>
             <Link className='p-3 rounded-md bg-lamaSkyLight' href='/'>
-              Teacher&apos;s Assignments
+              Student&apos;s Results
             </Link>
           </div>
         </div>
@@ -153,4 +153,4 @@ const SingleTeacherPage = () => {
   );
 };
 
-export default SingleTeacherPage;
+export default SingleStudentPage;
