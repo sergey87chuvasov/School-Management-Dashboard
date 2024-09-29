83b599846d0efa43b70f3434a82964af8f65e5dd
Sergey87 <serge.techno@gmail.com>
2024-09-30 00:07:58 +0300

    added Form Inputs with Validation

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -1,5 +1,6 @@
 import Announcements from '@/components/Announcements';
 import BigCalendar from '@/components/BigCalendar';
+import FormModal from '@/components/FormModal';
 import Performance from '@/components/Perfomans';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -20,7 +21,11 @@ const SingleTeacherPage = () => {
               />
             </div>
             <div className='w-2/3 flex flex-col justify-between gap-4'>
-              <h1 className='text-xl font-semibold'>Leonardo Snyder</h1>
+              <div className='flex items-center gap-4'>
+                <h1 className='text-xl font-semibold'>Leonardo Snyder</h1>
+                <FormModal table='teacher' type='update' data={{}} />
+              </div>
+
               <p className='text-sm text-gray-500'>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet
                 nostrum dolore quis porro tenetur? Veritatis esse officia.
